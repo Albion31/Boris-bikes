028c71235a3f1fee58041493205ea4c4f5920e24
Lars <larsfinlay@googlemail.com>
2017-11-24 15:44:44 +0000

    Refactoring chapter20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,8 @@ require "bike"
 
 describe DockingStation do
 
+  let(:bike) { double(:bike, :set_working= => true, :working? => true) }
+
   it { is_expected.to respond_to :release_bike}
 
   # it 'should get a bike' do
@@ -11,7 +13,6 @@ describe DockingStation do
   # end
 
   it 'docks a bike' do
-    bike = double( :bike, :set_working= => true, :working? => true )
     expect(subject.dock(bike)).to eq [bike]
   end
 
@@ -20,7 +21,6 @@ describe DockingStation do
   end
 
   it "should not accept more bikes than its capacity" do
-    bike = double( :bike, :set_working= => true, :working? => true )
     DockingStation::DEFAULT_CAPACITY.times{subject.dock(bike)}
     expect {subject.dock(bike)}.to raise_error("There are no capacity")
   end
@@ -36,19 +36,18 @@ describe DockingStation do
   end
 
   it "should accept broken bikes" do
-    bike = double( :bike, :set_working= => true, :working? => false )
+    allow(bike).to receive(:working?) {false}
     subject.dock(bike)
     expect(bike.working?).to eq false
   end
 
   it "should accept working bike without an argument" do
-    bike = double( :bike, :set_working= => true, :working? => true )
     subject.dock(bike)
     expect(bike.working?).to eq true
   end
 
   it "should not release broken bikes" do
-    bike = double( :bike, :set_working= => true, :working? => false )
+    allow(bike).to receive(:working?) {false}
     subject.dock(bike)
     expect{subject.release_bike}.to raise_error("Bike cannot be released as broken")
   end
